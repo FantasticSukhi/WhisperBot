39eb89c6f2bc9ee4db96367a9366bfdb603d724d
FantasticSukhi <122316561+FantasticSukhi@users.noreply.github.com>
2023-04-03 22:55:15 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -7,14 +7,14 @@ class Data:
 Hey {}.
 Welcome to {}
 
-I am the Master of Whisperers (like Varys in Game of Thrones).
+I am the Master of Whisperers messages means you can share hidden message to anyone .
 
 You can use me to send whispers to your friend in groups and channels (even if I'm not there).
 Only that friend and you will be able to read the message even though others are in same group. 
 
 To see how to use me press 'How to Use' below.
 
-By @StarkBots
+By @GORILLA_NETWORKS
     """
 
     # Home Button
@@ -31,24 +31,24 @@ By @StarkBots
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton("🎪 About 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
+        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/GORILLA_BOTS")],
+        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/GORRILA_SUPPORT")],
     ]
 
     # Help Message
     HELP = """
 Just type the message in below format in any chat.
 
-`@WhisperStarkBot your_message friend_username/id`
+`@WG_GBOT your_message friend_username/id`
     """
 
     # About Message
     ABOUT = """
 **About This Bot** 
 
-Bot created by @StarkBots
+Bot created by @GORILLA_NETWORK
 
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/WhisperBot)
+Source Code : [Click Here](https://github.com/FantasticSukhi/WhisperBot)
 
 Inspired By : nnbbot
 
@@ -56,5 +56,5 @@ Framework : [Pyrogram](docs.pyrogram.org)
 
 Language : [Python](www.python.org)
 
-Developer : @StarkProgrammer
-    """
+Developer : @MAMBA_RETURNS
+    
